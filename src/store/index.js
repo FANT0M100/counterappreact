8d8dfae9199633a14c99246fc3eb3b94dbf677d5
FANT0M100 <giorgie103188@gmail.com>
2024-02-20 13:44:13 +0400

    i redux toltik

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,33 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCountrer: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "icrement") {
     return {
       counter: state.counter + 1,
+      showCountrer: state.showCountrer,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCountrer: state.showCountrer,
     };
   }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.payload,
+      showCountrer: state.showCountrer,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      showCountrer: !state.showCountrer,
+      counter: state.counter,
+    };
+  }
+
   return state;
 };
 
